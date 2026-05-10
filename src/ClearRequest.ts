ac102bcd9c0f279ae22554a8311a9b0b526dd01b
3m1n3nc3 <mygames.ng@gmail.com>
2026-05-10 11:26:08 +0100

    feat(ClearRequest): extend middleware types to include Fastify, Hono, and Koa

--- src/ClearRequest.ts
+++ src/ClearRequest.ts
@@ -1,9 +1,15 @@
 import { Middleware as EMiddleware } from 'types/express'
+import { Middleware as FMiddleware } from 'types/fastify'
 import { Middleware as HMiddleware } from 'types/h3'
+import { Middleware as HNMiddleware } from 'types/hono'
+import { Middleware as KMiddleware } from 'types/koa'
 import { RequestData } from 'types'
 import { Route } from './Route'
 
-export class ClearRequest<X = any, M = HMiddleware | EMiddleware> {
+export class ClearRequest<
+    X = any,
+    M = HMiddleware | EMiddleware | FMiddleware | HNMiddleware | KMiddleware
+> {
     [key: string]: any
 
     /**
